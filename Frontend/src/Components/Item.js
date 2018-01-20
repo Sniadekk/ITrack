252a0440c288b66effab2eec61ead8022dc48c75
DESKTOP-CK5RREL\sniadek <wojokarolg9@gmail.com>
2018-01-20 16:12:26 +0100

    Started working on menu appearance after clicking on item. Added global event to disable menu with "esc". Few bug fixes.

--- Frontend/src/Components/Item.js
+++ Frontend/src/Components/Item.js
@@ -5,13 +5,28 @@ import '../Styles/App.css';
 class Item extends Component{
     constructor(props){
         super(props);
+        this.state = {menuToggled:false};
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.removeMenu = this.removeMenu.bind(this);
     }
+
+    toggleMenu(){
+        document.addEventListener('keydown',this.removeMenu);
+        this.setState({menuToggled:true});
+    }
+
+    removeMenu(evt){
+        console.log('click');
+        if(evt.keyCode === 27){
+            this.setState({menuToggled:false});
+        }
+    }
+
     render(){
         return(
             <div className="item">
-
-                <p>{this.props.data.name}</p>
-                <p>{this.props.data.amount}</p>
+                <button onClick ={this.toggleMenu} className="button primary ">{this.props.data.name}</button>
+                {this.state.menuToggled && <div tabIndex="0" onKeyDown={this.removeMenu} className="menu">MENU</div>}
 
             </div>
 
